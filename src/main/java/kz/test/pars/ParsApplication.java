d44f1f75021bb5da621aa16dbee0a7f8604b868b
a.bisen <sacrxsanct@outlook.com>
2024-02-27 17:28:00 +0600

    init

---
+++ src/main/java/kz/test/pars/ParsApplication.java
@@ -0,0 +1,13 @@
+package kz.test.pars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsApplication.class, args);
+	}
+
+}
